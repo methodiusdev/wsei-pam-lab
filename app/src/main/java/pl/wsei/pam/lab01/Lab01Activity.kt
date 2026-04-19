f028ecb9a1e39dfdeaa03e96d15a2d7f99178888
methodiusdev <methodius.dev@gmail.com>
2026-04-19 21:46:17 +0200

    This reworks the labs so that there is a main screen which redirects to each particular lab

--- app/src/main/java/pl/wsei/pam/lab01/Lab01Activity.kt
+++ app/src/main/java/pl/wsei/pam/lab01/Lab01Activity.kt
@@ -9,7 +9,7 @@ import android.widget.TextView
 import android.widget.Toolbar.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Lab01Activity : AppCompatActivity() {
     lateinit var mLayout: LinearLayout
     lateinit var mTitle: TextView
     lateinit var mProgress: ProgressBar
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     var mButtons: MutableList<Button> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lab01)
         mLayout = findViewById(R.id.main)
 
         mTitle = TextView(this)
